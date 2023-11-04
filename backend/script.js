6540551247ab81edd15cd90be101881c8841ceff
yashdarshankar <yashhdarshankar@gmail.com>
2023-11-04 23:27:03 +0530

    updated

--- backend/script.js
+++ backend/script.js
@@ -1,19 +1,16 @@
-// Import required modules
 const express = require('express');
 const cors = require('cors');
 const { MongoClient } = require('mongodb');
 const path = require('path');
 const bodyParser = require('body-parser');
 
-// Create an Express application
 const app = express();
 const port = process.env.PORT || 3001;
 
 app.use(cors());
 app.use(bodyParser.json());
 
-// MongoDB Connection
-const uri = 'mongodb+srv://yashdarshankar:Yash%401001@cluster0.gto09ok.mongodb.net/mernstack?retryWrites=true&w=majority'; // Replace with your MongoDB URI
+const uri = 'mongodb+srv://yashdarshankar:Yash%401001@cluster0.gto09ok.mongodb.net/mernstack?retryWrites=true&w=majority';
 const client = new MongoClient(uri, { useUnifiedTopology: true });
 
 async function connectToMongoDB() {
@@ -27,13 +24,12 @@ async function connectToMongoDB() {
 
 connectToMongoDB();
 
-// Define API endpoints
 app.get('/api/qrcodesfetch', async (req, res) => {
   try {
-    const db = client.db('mernstack'); // Replace with your database name
+    const db = client.db('mernstack');
     const qrCodeCollection = db.collection('temp');
     const data = await qrCodeCollection.find({}).toArray();
-    console.log("fetched the data",data)
+    console.log("fetched the data", data)
     res.json(data);
   } catch (error) {
     res.status(500).json({ error: 'Error retrieving data' });
@@ -42,23 +38,23 @@ app.get('/api/qrcodesfetch', async (req, res) => {
 
 app.post('/api/qrcodes', async (req, res) => {
   try {
-    const db = client.db('mernstack'); // Replace with your database name
+    const db = client.db('mernstack');
     const qrCodeCollection = db.collection('temp');
-    const { content} = req.body;
-    const result = await qrCodeCollection.insertOne({ content});
+    const { content } = req.body;
+    const result = await qrCodeCollection.insertOne({ content });
     console.log("posted the data")
     res.json({ success: true, id: result.insertedId });
   } catch (error) {
     res.status(500).json({ error: 'Error saving data' });
   }
 });
-// Delete all QR codes
+
 app.delete('/api/qrcodesdelete', async (req, res) => {
   try {
-    const db = client.db('mernstack'); // Replace with your database name
+    const db = client.db('mernstack');
     const qrCodeCollection = db.collection('temp');
-    
-    // Use the deleteMany method to delete all documents in the collection
+
+
     const result = await qrCodeCollection.deleteMany({});
 
     if (result.deletedCount > 0) {
@@ -78,7 +74,7 @@ app.use(express.static(path.join(__dirname, '../frontend/build')));
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../frontend/build', 'index.html'));
 });
-// Start the Express server
+
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
